5260316dfa966d7099542d3db7303e62bc92fac8
chenc118 <chenc118@my.yorku.ca>
2021-11-25 08:53:04 -0500

    Add option to click element by css selector
    -maybe more robust than just clicking w/e mouse is in case it's not in
    the exact right place

--- src/script/action/MouseClick.java
+++ src/script/action/MouseClick.java
@@ -1,5 +1,6 @@
 package script.action;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.interactions.PointerInput;
 
@@ -12,20 +13,35 @@ import clients.SeleniumClient;
  *
  */
 public class MouseClick extends Action {
-
+	
+	private String css = "";
+	
 	public MouseClick(String raw) {
 		super(raw);
+		//format is click [css]
+		//extracts css as w/e is after the space
+		int space = raw.indexOf(' ');
+		if(space>0) {
+			css = raw.substring(space+1);
+		}
 	}
 
 	@Override
 	public Action execute(Client client) {
 		if (client instanceof SeleniumClient) {
 			SeleniumClient sClient = (SeleniumClient) client;
-			Actions action = new Actions(sClient.getWebDriver());
-			action.tick(sClient.getPointerInput().createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
-			action.tick(sClient.getPointerInput().createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
-			action.perform();
+			if(css.equals("")) {
+				Actions action = new Actions(sClient.getWebDriver());
+				action.tick(sClient.getPointerInput().createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+				action.tick(sClient.getPointerInput().createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+				action.perform();
+			}
+			else {
+				//click using the css selector
+				sClient.getWebDriver().findElement(By.cssSelector(css)).click();
+			}
 		}
+			
 		return super.next;
 	}
 
