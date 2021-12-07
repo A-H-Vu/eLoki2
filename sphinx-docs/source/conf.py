f0466348a3e80db6dd9013af5d1e1bb074952363
chenc118 <chenc118@my.yorku.ca>
2021-12-07 17:51:53 -0500

    Bump version

--- sphinx-docs/source/conf.py
+++ sphinx-docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Allen Chen'
 author = 'Allen Chen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.6'
+release = '0.2.7'
 
 
 # -- General configuration ---------------------------------------------------
