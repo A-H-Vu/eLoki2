9c5b4b93d6e546602f5ddcdb1230b55cec77c4d6
chenc118 <chenc118@my.yorku.ca>
2021-12-09 20:12:49 -0500

    Fix issue with firefox by using HasExtensions

--- src/clients/SeleniumFirefox.java
+++ src/clients/SeleniumFirefox.java
@@ -1,17 +1,21 @@
 package clients;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.HasExtensions;
 import org.openqa.selenium.remote.CapabilityType;
 
 public class SeleniumFirefox extends SeleniumClient {
 	private FirefoxOptions options;
 	private FirefoxProfile profile;
+	private ArrayList<Path> extensionPaths = new ArrayList<Path>();
 	
 	public SeleniumFirefox() {
 		super("Selenium-Firefox");
@@ -46,11 +50,19 @@ public class SeleniumFirefox extends SeleniumClient {
 	}
 	
 	public void addExtension(File path) {
+		extensionPaths.add(path.toPath());
 		profile.addExtension(path);
 	}
 	
 	public void init() {
+		options.setProfile(profile);
 		super.webdriver = new FirefoxDriver(options);
+		//Installing via profile.addExtension does not work as 3.141.59 seems to have a bug
+		//where it is unable to load the profile thus not loading the extension
+		HasExtensions he = (HasExtensions)webdriver;
+		for(Path p:extensionPaths) {
+			he.installExtension(p);
+		}
 	}
 	
 	
