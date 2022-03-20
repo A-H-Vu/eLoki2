8d84e9d848a7834f1af30804543279448bd7b5c4
chenc118 <chenc118@my.yorku.ca>
2022-03-20 11:39:19 -0400

    Bump version

--- src/core/Main.java
+++ src/core/Main.java
@@ -42,7 +42,7 @@ import script.mod.SimpleRandomMove;
 
 public class Main {
 	//Version string, should be same as the version string in the pom.xml file
-	private static String version = "0.4.2";
+	private static String version = "0.5.0";
 	//Class that runs scripts, for now only create one as default, in the future one will be used on each thread
 	public static ScriptController defaultController = new ScriptController();
 	
